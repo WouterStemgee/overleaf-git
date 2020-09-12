e00dfd1b01cdda3591778b60aa6c97e36e0940cd
Wouter Stemgée <wouter.stemgee@ugent.be>
2020-09-12 12:08:13 +0200

    Added logging and external config support

--- app.py
+++ app.py
@@ -8,18 +8,34 @@ from datetime import datetime
 import zipfile
 import logging
 from urllib.error import HTTPError
+from configparser import ConfigParser
 
-logger = logging.getLogger(__name__)
 
-OVERLEAF_PROJECT_URI = ''
-GIT_REPOSITORY_URI = ''
+logger = logging.getLogger('overleaf_git')
+logger.setLevel(logging.INFO)
+ch = logging.StreamHandler()
+ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(ch)
+
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='CLI tool to automate the process of saving Overleaf projects to a Git repository.')
     args = parser.parse_args()
 
+    # parse configuration variables
+    logger.info('Parsing configuration variables')
+    config = ConfigParser()
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.ini')
+    if not os.path.isfile(config_path):
+        logger.exception('Configuration error: configuration file not found.')
+    config.read(config_path)
+
+    OVERLEAF_PROJECT_URI = config['overleaf']['overleaf_project_url'] 
+    GIT_REPOSITORY_URI = config['git']['git_repository_url'] 
+
     # download overleaf .zip file to a temporary directory (session cookie from browser required for authentication)
+    logger.info('Download overleaf .zip file to a temporary directory')
     try:
         dir_name = 'data'
         if not os.path.exists(dir_name):
@@ -33,7 +49,7 @@ if __name__ == '__main__':
         urllib.request.install_opener(opener)
         url = OVERLEAF_PROJECT_URI + '/download/zip'
 
-        with urllib.request.urlopen(url) as response, open(path, 'w') as out_file:
+        with urllib.request.urlopen(url) as response, open(path, 'wb') as out_file:
             shutil.copyfileobj(response, out_file)
     except HTTPError:
         logger.exception('Authentication error: please login to overleaf in your browser first.')
